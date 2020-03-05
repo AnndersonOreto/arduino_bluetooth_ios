7afbb134595f489fcf10d474ae2baf67198d60d3
Anderson <anderlentz@gmail.com>
2020-03-05 16:49:00 -0300

    Animating circle progress bar

---
+++ Geloou/Geloou/CircularProgressView.swift
@@ -0,0 +1,23 @@
+//
+//  CircularProgressView.swift
+//  Geloou
+//
+//  Created by Anderson on 05/03/20.
+//  Copyright © 2020 Academy. All rights reserved.
+//
+
+import UIKit
+
+class CircularProgressView: UIView {
+
+    let progressContainer: UIView = {
+        let view = UIView()
+        return view
+    }()
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+}
--- Geloou/Geloou/ViewController.swift
+++ Geloou/Geloou/ViewController.swift
@@ -9,12 +9,87 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let iceColor = UIColor.init(red: 32/255, green: 195/255, blue: 208/255, alpha: 1).cgColor
+    let trackLayerColor = UIColor.init(red: 185/255, green: 232/255, blue: 234/255, alpha: 1).cgColor
+    let pulsingColor = UIColor.init(red: 134/255, green: 214/255, blue: 216/255, alpha: 0.7).cgColor
+    let circularBackgroundColor = UIColor.white.cgColor
+    
+    lazy var shapeLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        
+        
+        
+        
+        return layer
+    }()
+    
+    var pulsatingLayer: CAShapeLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let trackLayer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero,
+                                        radius: 100,
+                                        startAngle: 0,
+                                        endAngle: 2 * CGFloat.pi,
+                                        clockwise: true)
+        
+        pulsatingLayer = CAShapeLayer()
+        pulsatingLayer.path = circularPath.cgPath
+        pulsatingLayer.strokeColor = UIColor.clear.cgColor
+        pulsatingLayer.lineWidth = 10
+        pulsatingLayer.fillColor = pulsingColor
+        pulsatingLayer.position = view.center
+        view.layer.addSublayer(pulsatingLayer)
+        animatePulsatingLayer()
+        
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = trackLayerColor
+        trackLayer.lineWidth = 10
+        trackLayer.fillColor = circularBackgroundColor
+        trackLayer.position = view.center
+        view.layer.addSublayer(trackLayer)
+        
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = iceColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.lineCap = .round
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.position = view.center
+        shapeLayer.strokeEnd = 0 // how many in porcentage is completed (0.0 to 1). CGFloat
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1) // rotate -90 degrees
+        view.layer.addSublayer(shapeLayer)
+  
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
+        
     }
 
+    fileprivate func startCircleAnimation() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        shapeLayer.strokeEnd = 0 // start on o'clock position. How many in porcentage is completed (0.0 to 1). CGFloat
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2 //in seconds
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "doAnimation")
+    }
+    
+    @objc private func handleTap() {
+        startCircleAnimation()
+    }
 
+    func animatePulsatingLayer() {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.toValue = 1.25
+        animation.duration = 1
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        pulsatingLayer.add(animation, forKey: "pulsingLayer")
+    }
 }
 
