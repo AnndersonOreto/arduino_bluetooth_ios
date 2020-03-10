04a827aecf548886770cc1ea1f8d98c2c827338c
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2020-03-10 13:59:05 -0300

    Arduino communicator

---
+++ Geloou/Geloou/Arduino/ArduinoCommunicator.swift
@@ -0,0 +1,9 @@
+//
+//  ArduinoCommunicator.swift
+//  Geloou
+//
+//  Created by Marcus Vinicius Vieira Badiale on 10/03/20.
+//  Copyright © 2020 Academy. All rights reserved.
+//
+
+import Foundation
